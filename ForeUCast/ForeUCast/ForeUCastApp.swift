6ad15205257f8ba7802cd401eb289966f44c5734
Vigneshwaran Murugesan <mvigneshwaran1990@gmail.com>
2023-05-27 23:15:34 -0400

    Commit after removing submodule issue.

---
+++ ForeUCast/ForeUCast/ForeUCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForeUCastApp.swift
+//  ForeUCast
+//
+//  Created by Vigneshwaran Murugesan on 27/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ForeUCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
